61cbc6bb842bf17a456ad81fb2a09a85c10a5fef
aidangehring <spsagehr@ljmu.ac.uk>
2026-04-24 19:06:55 +0100

    add RPE

--- utils/config.py
+++ utils/config.py
@@ -21,6 +21,7 @@ Shoes= {
         'image': 'images/adidas_spezial',
         'standing_comfort': 7,
         'running_comfort': 3,
+        'RPE': 7,
         'comfort comments': 'The Adidias Spezial was comfortable to wear for standing, but during running my shins began to feel sore and I felt like I\
             had less support compared to the other shoes.',
         'color': "#1737C4"
@@ -31,6 +32,7 @@ Shoes= {
         'image': 'images/nike_relentless_2.jpg',
         'standing_comfort': 6,
         'running_comfort': 7,
+        'RPE': 6,
         'comfort comments': 'The Nike Relentless 2 was comfortable to wear for standing, and provided good support during running.\
               However, I felt like the shoe was quite stiff and not as comfortable as other shoes during standing.',
         'color': "#C41717"
@@ -41,6 +43,7 @@ Shoes= {
         'image': 'images/pegasus_plus',
         'standing_comfort': 7,
         'running_comfort': 8,
+        'RPE': 5,
         'comfort comments': 'The Nike Pegasus Plus was comfortable to wear for standing, and felt very nice to run in. \
                 I felt like the shoe provided good cushioning, though felt a little bit unstable at first while standing.',
         'color':"#17C420"
